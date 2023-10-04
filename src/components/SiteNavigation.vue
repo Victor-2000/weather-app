fae6d3669b879fcb00453babeaa43ed331ffb796
Victor <17victor.c@gmail.com>
2023-10-04 13:29:40 +0100

    Created an information modal component

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -8,11 +8,12 @@
                 </div>
             </RouterLink>
             <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                @click="toggleModal"></i>
                 <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
             </div>
-
-            <BaseModal>
+            
+            <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
                 <div class="text-black">
                     <h1 class="text-2xl mb-1">About:</h1>
                     <p class="mb-4">
@@ -22,20 +23,20 @@
                     <h2 class="text-2xl">How it works:</h2>
                     <ol class="list-decimal list-inside mb-4">
                         <li>
-                        Search for your city by entering the name into the
-                        search bar.
+                            Search for your city by entering the name into the
+                            search bar.
                         </li>
                         <li>
-                        Select a city within the results, this will take
-                        you to the current weather for your selection.
+                            Select a city within the results, this will take
+                            you to the current weather for your selection.
                         </li>
                         <li>
-                        Track the city by clicking on the "+" icon in the
-                        top right. This will save the city to view at a
-                        later time on the home page.
+                            Track the city by clicking on the "+" icon in the
+                            top right. This will save the city to view at a
+                            later time on the home page.
                         </li>
                     </ol>
-
+                    
                     <h2 class="text-2xl">Removing a city</h2>
                     <p>
                         If you no longer wish to track a city, simply select
@@ -49,6 +50,11 @@
 </template>
 
 <script setup>
-import {RouterLink} from 'vue-router'
+import {ref} from 'vue';
+import {RouterLink} from 'vue-router';
 import BaseModal from './BaseModal.vue';
+
+const modalActive = ref(null);
+const toggleModal = () => modalActive.value = !modalActive.value;
+
 </script>
