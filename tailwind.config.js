d0ed3001b4421e74b7cc95efc77d4787f3538784
Victor <17victor.c@gmail.com>
2023-10-04 11:30:36 +0100

    Set up Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "weather-primary": "#0068A",
+        "weather-secondary": "#004E71",
+      }
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    container:{
+      padding: "2rem",
+      center: true,
+    }
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    }
   },
   plugins: [],
 }
