5598049f9473b6dce11e80e80902133786407fbd
Victor <17victor.c@gmail.com>
2023-10-05 15:24:53 +0100

    Implemented lazy search with Mapbox API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,61 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input 
+      type="text" 
+      v-model="searchQuery"
+      @input="getSearchResults"
+      placeholder="Search for a city"
+      class="py-2 px-1 w-full bg-transparent border-b 
+      focus:border-weather-secondary focus:outline-none 
+      focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <p v-if="searchError"> 
+          Sorry, something went wrong, please try again. 
+        </p>
+        <p v-if="!serverError && mapboxSearchResults.length === 0 "> 
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
+          class="py-2 cursor-pointer">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+ import {ref} from 'vue';
+ import axios from 'axios';
+ import config from 'config';
+
+ const mapboxAPIKey = config.MAPBOX_API_KEY;
+ const searchQuery = ref("");
+ const queryTimeout = ref(null);
+ const mapboxSearchResults = ref(null);
+
+ const getSearchResults = () => {
+    clearTimeout(queryTimeout.value);
+    queryTimeout = setTimeout(async () => {
+      if(searchQuery.value !== ""){
+        try {
+          const result = await axios.get(
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+          );
+          mapboxSearchResults.value = result.data.features;
+        } catch {
+          searchError.value = true;
+        }
+      }else{
+        mapboxSearchResults.value = null;
+      }
+    }, 300);
+ }
+</script>
