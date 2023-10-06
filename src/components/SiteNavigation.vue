59ace45370125685e537e9518dc68b4c4d590b50
Victor <17victor.c@gmail.com>
2023-10-06 12:37:12 +0100

    Save city to local storage

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -10,9 +10,12 @@
             <div class="flex gap-3 flex-1 justify-end">
                 <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
                 @click="toggleModal"></i>
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i 
+                class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                @click="addCity"
+                v-if="!route.query.preview"
+                ></i>
             </div>
-            
             <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
                 <div class="text-black">
                     <h1 class="text-2xl mb-1">About:</h1>
@@ -51,10 +54,39 @@
 
 <script setup>
 import {ref} from 'vue';
-import {RouterLink} from 'vue-router';
+import {RouterLink, useRoute, useRouter} from 'vue-router';
 import BaseModal from './BaseModal.vue';
+import {uid} from 'uid';
 
 const modalActive = ref(null);
 const toggleModal = () => modalActive.value = !modalActive.value;
+
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
+const addCity = () => {
+    if(localStorage.getItem('savedCities')){
+        savedCities.value = JSON.parse(
+            localStorage.getItem('savedCities')
+        );
+    }
+
+    const locationObj = { 
+        id: uid(),
+        state: route.params.state,
+        city: route.params.city,
+        coords:{
+            lat: route.query.lat,
+            lon: route.query.lon,
+        }
+    };
+
+    savedCities.value.push(locationObj);
+    localStorage.setItem('savedCities', JSON.stringify(savedCities.value));
+
+    let query = Object.assign({}, route.query);
+    delete query.preview;                                                   
+    router.replace({ query });
+};
 
 </script>
